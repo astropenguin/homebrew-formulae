a66244bff4c1501f9914a3bae14291a2924c6dec
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2018-12-13 22:54:45 +0900

    :+1: Update radex (v0.1.2)

--- Formula/radex.rb
+++ Formula/radex.rb
@@ -1,13 +1,13 @@
 class Radex < Formula
   desc "A computer program to calculate the strengths of atomic and molecular lines from interstellar clouds"
   homepage "https://personal.sron.nl/~vdtak/radex/index.shtml"
-  url "https://github.com/astropenguin/radex-install/archive/v0.1.1.tar.gz"
-  sha256 "55d1cd6ae06f3eeca72f5f81799c7f047cbe6477e4af107b64ed8a826464c42b"
+  url "https://github.com/astropenguin/radex-install/archive/v0.1.2.tar.gz"
+  sha256 "30b0eb0c6f919350979358b4c13f0f622c657df1af10023449b39f30cd0d675c"
 
   depends_on "gcc" => :build
 
   def install
-    system "make", "build", "-j", "1"
+    system "make", "build"
     bin.install "radex-sphere"
     bin.install "radex-lvg"
     bin.install "radex-slab"
