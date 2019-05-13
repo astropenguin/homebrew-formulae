6c01bb8a6f407d49e4daa386f81a1fcf1f974020
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-05-13 16:59:27 +0900

    :+1: Update radex (v0.3.1)

--- Formula/radex.rb
+++ Formula/radex.rb
@@ -1,13 +1,16 @@
 class Radex < Formula
   desc "Non-LTE molecular radiative transfer in homogeneous interstellar clouds"
   homepage "https://personal.sron.nl/~vdtak/radex/index.shtml"
-  url "https://github.com/astropenguin/radex-install/archive/v0.2.1.tar.gz"
-  sha256 "f4d3e2d0f239f344850812c1eae2e61a22deabaa4d182cc86c94d8415b383e1f"
+  url "https://github.com/astropenguin/radex-install/archive/v0.3.1.tar.gz"
+  sha256 "5ac2ae03eeab8847c7f65dc4e2768e6fc8a95b184e90ff5ecb0baa3eab71edfb"
 
   depends_on "gcc" => :build
 
   def install
-    ENV["MOLDAT"] = ENV["HOMEBREW_MOLDAT"]
+    ENV["DATADIR"] = ENV["HOMEBREW_DATADIR"]
+    ENV["LOGFILE"] = ENV["HOMEBREW_LOGFILE"]
+    ENV["MINITER"] = ENV["HOMEBREW_MINITER"]
+    ENV["MAXITER"] = ENV["HOMEBREW_MAXITER"]
     system "make", "build"
     bin.install "radex-uni"
     bin.install "radex-lvg"
