65c0469e0cbd5e2720ec52b72c6876faca055bc2
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-12-20 21:52:15 +0900

    Update homepage of BBarolo

--- Formula/bbarolo.rb
+++ Formula/bbarolo.rb
@@ -1,6 +1,6 @@
 class Bbarolo < Formula
   desc "3D fitting tool to derive the kinematics of galaxies from emission-line observations"
-  homepage "http://editeodoro.github.io/Bbarolo/"
+  homepage "https://editeodoro.github.io/Bbarolo"
   url "https://github.com/editeodoro/Bbarolo/releases/download/1.5/BBarolo_MacOSX.zip"
   sha256 "85ec68a5b154226d5a801fbd0a08ea6db85843bfbe33a9097cbf64777e625d06"
 
