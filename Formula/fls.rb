cbabede7b3035138e4c5a683ca6cdbb346666a95
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-12-28 18:53:21 +0900

    Add fls (v0.2)

---
+++ Formula/fls.rb
@@ -0,0 +1,26 @@
+class Fls < Formula
+  desc "List paths of current macOS Finder windows"
+  homepage "https://github.com/astropenguin/fls"
+  url "https://github.com/astropenguin/fls/archive/v0.2.tar.gz"
+  sha256 "10b81121b350e8ae9d4067ed3e11aad17d833a0dda4fd26529896c7673718675"
+
+  depends_on "fzf"
+
+  def install
+    bin.install "fls"
+    etc.install "fcd.sh"
+    etc.install "fcd.fish"
+  end
+
+  def caveats
+    <<~EOS
+      To enable fcd, add to your shell profile:
+        (bash) source #{etc}/#{prefix}/fcd.sh
+        (fish) source #{etc}/#{prefix}/fcd.fish
+    EOS
+  end
+
+  test do
+    system "true"
+  end
+end
