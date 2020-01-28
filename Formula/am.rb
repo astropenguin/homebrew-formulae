18f2b7421808fdd48acaa74d10f0650f90341e1c
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-01-29 07:47:56 +0900

    Update am (v11.0)

--- Formula/am.rb
+++ Formula/am.rb
@@ -1,8 +1,8 @@
 class Am < Formula
   desc "Tool for radiative transfer computations at microwave to submillimeter wavelengths"
   homepage "https://www.cfa.harvard.edu/~spaine/am/index.html"
-  url "https://zenodo.org/record/1193771/files/am-10.0.tgz"
-  sha256 "cac612c112ff4c5386570088ba90d2f6469b013cd683e712a4b27ea0470f806c"
+  url "https://zenodo.org/record/3406483/files/am-11.0.tgz"
+  sha256 "2b0ba95eab759f8813f517197a77c52c4e1fe36379ed3facec5941c0fa552995"
 
   depends_on "gcc" => :build
 
